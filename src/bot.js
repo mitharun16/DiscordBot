c523d09e55c4bf5d3948c0ac98da661dbfce7bf4
Tharun Imayavaramban <mitharun16@gmail.com>
2023-01-01 16:44:57 -0700

    Cleaned up

--- src/bot.js
+++ src/bot.js
@@ -1,35 +1,66 @@
-require('dotenv').config()
+const Discord = require("discord.js")
+const dotenv = require("dotenv")
+const { REST } = require("@discordjs/rest")
+const { Routes } = require("discord-api-types/v9")
+const fs = require("fs")
+const { Player } = require("discord-player")
 
+dotenv.config()
+const TOKEN = process.env.token
 
-const prefix = '!'
+const LOAD_SLASH = process.argv[2] == "load"
+
+const CLIENT_ID = "1056755927862227065"
 const {Client,GatewayIntentBits, Collection, EmbedBuilder, PermissionsBitField} = require('discord.js')
+const client = new Client({intents:[GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildVoiceStates]})
 
-const client = new Client({intents:[GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]})
-const { token } = process.env
-client.on('ready', ()=> {
-    console.log(`Logged in as ${client.user.tag}!`);
-
-})
-client.on("messageCreate", (message) => {
-    if(!message.content.startsWith(prefix) || message.author.bot)
-        return
-
-
-    const args = message.content.slice(prefix.length).split(/ +/)
-    const command = args.shift().toLowerCase();
-
-
-    // message array
-
-    const messageArray = message.content.split(" ")
-    const argument = messageArray.slice(1)
-    const cmd = messageArray[0];
-
-    if(command === 'play')
-    {
-        message.channel.send("Hello")        
-
+client.slashcommands = new Discord.Collection()
+client.player = new Player(client, {
+    ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25
     }
 })
 
-client.login(process.env.token)
+let commands = []
+
+const slashFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js"))
+for (const file of slashFiles){
+    const slashcmd = require(`./slash/${file}`)
+    client.slashcommands.set(slashcmd.data.name, slashcmd)
+    if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
+}
+
+if (LOAD_SLASH) {
+    const rest = new REST({ version: "9" }).setToken(TOKEN)
+    console.log("Deploying slash commands")
+    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commands})
+    .then(() => {
+        console.log("Successfully loaded")
+        process.exit(0)
+    })
+    .catch((err) => {
+        if (err){
+            console.log(err)
+            process.exit(1)
+        }
+    })
+}
+else {
+    client.on("ready", () => {
+        console.log(`Logged in as ${client.user.tag}`)
+    })
+    client.on("interactionCreate", (interaction) => {
+        async function handleCommand() {
+            if (!interaction.isCommand()) return
+
+            const slashcmd = client.slashcommands.get(interaction.commandName)
+            if (!slashcmd) interaction.reply("Not a valid slash command")
+
+            await interaction.deferReply()
+            await slashcmd.run({ client, interaction })
+        }
+        handleCommand()
+    })
+    client.login(TOKEN)
+}
